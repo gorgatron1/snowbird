61efe696724436d808ac63b8ed532963b276249f
David Koski <dkoski@apple.com>
2020-01-12 19:46:56 -0800

    collapse history

---
+++ tools/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+guilds=(
+	'report 564121831'
+)
+	
+for g in $guilds ; do 
+	args=("${(@s/ /)g}")
+	echo "==============="
+	echo "$args[1]"
+	
+	echo "FETCH: $guild"
+	echo "python ~/bin/swgoh.help.py $args[2] $args[1]"
+	
+	python ~/bin/swgoh.help.py $args[2] $args[1] || (sleep 60 && python ~/bin/swgoh.help.py $args[2] $args[1]) || (sleep 120 && python ~/bin/swgoh.help.py $args[2] $args[1]) || exit 1
+	
+	time ~/bin/swgoh --guild $args[1].json --unbrand --site docs 
+#	sleep 60
+done
+
+echo "==============="
+echo "ALLIANCE"
+
+# time ~/bin/swgoh --alliance docs *.json
